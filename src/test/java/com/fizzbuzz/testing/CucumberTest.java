8d95405d11f7b71cd2e4892603f1f843b855ff20
Jaisurya26 <jaisuryaindu.3112@gmail.com>
2022-02-15 13:39:51 +0530

    first commit

---
+++ src/test/java/com/fizzbuzz/testing/CucumberTest.java
@@ -0,0 +1,14 @@
+package com.fizzbuzz.testing;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features="src/test/resources",
+        glue = "com.fizzbuzz.testing",
+        plugin = {"json:target/cucumber.json"}
+)
+public class CucumberTest {
+}
---
+++ src/test/java/com/fizzbuzz/testing/StepDef.java
@@ -0,0 +1,26 @@
+package com.fizzbuzz.testing;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDef {
+    @Given("Create a FizzBuzz game play")
+    public void create_a_fizz_buzz_game_play() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I play with number {int}")
+    public void i_play_with_number(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The result is {string}")
+    public void the_result_is(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
